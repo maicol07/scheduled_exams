1d1f310d3b687e6e0efb14ee7fcca06bc0c4abab
Maicol <maicolbattistini@live.it>
2018-07-08 15:48:53 +0200

    Inserito un file separato per il cambiamento dello sfondo

---
+++ layout/header/background-change.php
@@ -0,0 +1,45 @@
+<?php
+$alba = 6;
+$giorno = 18;
+$ora = date("H");
+?>
+<?php if ($ora >= 3 && $ora <= $alba) { ?>
+    <!-- Se l'ora attuale è maggiore di 3 e minore di $alba che è 6 -->
+    <style>
+        /* Mostra questo codice css per l'alba */
+        body {
+            background-image: url(https://dl.dropboxusercontent.com/s/6m36z4d8zyi8ily/3%20-%20vVsVx5p.png?dl=0);
+            background-position: center;
+            background-size: auto;
+            background-repeat: repeat;
+        }
+    </style>
+
+<?php } elseif ($ora > $alba && $ora <= $giorno) { ?>
+
+    <!-- Se l'ora attuale è maggiore di 6 ($alba) e minore di $giorno che è 18 -->
+    <style>
+        /* Mostra questo codice css per il giorno */
+        body {
+            background-image: url(https://dl.dropboxusercontent.com/s/ka8x2wcs46y03kd/4%20-%20ZFabsbM.png?dl=0);
+            background-position: center;
+            background-size: auto;
+            background-repeat: repeat;
+        }
+    </style>
+
+<?php } else { ?>
+
+    <!-- Se nessuna delle precendenti condizioni è soddisfatta allora è notte -->
+
+    <style>
+        /* Mostra questo codice css per la notte */
+        body {
+            background-image: url(https://dl.dropboxusercontent.com/s/0qe4q8rl7pfspal/2%20-%20lGi5EO6.png?dl=0);
+            background-position: center;
+            background-size: auto;
+            background-repeat: repeat;
+        }
+    </style>
+
+<?php } ?>
