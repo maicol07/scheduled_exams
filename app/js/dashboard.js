62f9a893766f4752229b3cb2da7d58ab794d43fe
maicol07 <maicolbattistini@live.it>
2018-08-10 18:07:32 +0200

    File layout dashboard + inizializzazione (comune a tutte le pagine)

---
+++ app/js/dashboard.js
@@ -0,0 +1,60 @@
+var $root = $('html, body');
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $root.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+function crea_classe() {
+    swal({
+        title: "Crea classe",
+        text: "Inserisci il nome della classe",
+        input: "text",
+        confirmButtonText: "CREA!",
+        cancelButtonText: "Annulla",
+    }).then(name => {
+        if (!name.value) throw null;
+
+        if (window.XMLHttpRequest) {
+            // code for modern browsers
+            var xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for old IE browsers
+            var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var column = document.createElement("DIV");
+                column.className += "col s6 m2 l3";
+                var container = document.createElement("DIV"); // Create a <div> element
+                container.className += "card activator hoverable waves-effect waves-light divlink"; // Add card class
+                container.setAttribute("onlick", "window.location=\"classe.php?id=" + this.responseText + "\""); // Add link to div
+                var cardcontent = document.createElement("DIV");      // Create a <div> element
+                cardcontent.className += "card-action"; // Add CSS class
+                var cardtext = document.createTextNode(name.value); // Create a text node
+                cardcontent.appendChild(cardtext); // Append the text node to <div>
+                container.appendChild(cardcontent); // Append the <div> element to <div>
+                column.appendChild(container); // Append <div> element to <div>
+                document.getElementById("rigaclassi").appendChild(column);           // Append <div> to <div> with id="rigaclassi"
+                swal({
+                    title: "Classe creata!",
+                    text: "La classe " + name.value + " è stata creata!",
+                    type: "success",
+                });
+            }
+        };
+
+        xmlhttp.open("GET", "includes/addclass.php?classe=" + name.value + "&userID=" + userID, true);
+        xmlhttp.send();
+
+    }).catch(err => {
+        if (err) {
+            swal("Oh no!", "Si è verificato un errore!", "error");
+        } else {
+            swal.disableLoading();
+            swal.close();
+        }
+    });
+}
--- app/js/init.js
+++ app/js/init.js
@@ -1,19 +1,44 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
-    $(".dropdown-trigger").dropdown({
+    $("#dd-trigger-profile").dropdown({
         inDuration: 300,
         outDuration: 225,
         constrainWidth: false, // Does not change width of dropdown to that of the activator
         hover: false, // Activate on hover
         coverTrigger: false, // Displays dropdown below the button
-        alignment: 'right' // Displays dropdown with edge aligned to the left of button);
-    })
+        alignment: 'right' // Displays dropdown with edge aligned to the right of button);
+    });
+    $("#dd-trigger-classi").dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        coverTrigger: false, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the right of button);
+    });
+    $('.collapsible').collapsible();
 });
-var $root = $('html, body');
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
 
-    $root.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+
+function info() {
+    swal({
+        title: "Informazioni",
+        html: 'Interrogazioni Programmate è un software closed-source sviluppato da ' +
+            '<a href="https://www.maicol07.it" target="_blank">maicol07</a>.<h5>Link utili</h5>' +
+            '<ul>' +
+            '<li><a href="https://community.interrogazioniprogrammate.tk">Community</a></li>' +
+            '<li><a href="https://interrogazioniprogrammate.tk/community/roadmap.php">Roadmap</a></li>' +
+            '</ul>' +
+            '<h5>Riconoscimenti</h5>Icona creata da ' +
+            '<a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> di ' +
+            '<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> ha licenza ' +
+            '<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>' +
+            '<h5>Script e librerie di terze parti</h5><ul>' +
+            '<li><a href="https://materializecss.com">Materialize CSS</a></li>' +
+            '<li><a href="https://jquery.com/">JQuery</a></li>' +
+            '<li><a href="https://sweetalert.js.org/">SweetAlert</a> e <a href="https://sweetalert2.github.io/">SweetAlert 2</a></li>' +
+            '<li><a href="https://github.com/taylorhakes/promise-polyfill">Promise Polyfill</a></li>' +
+            '</ul>',
+        type: "info",
+    })
+}
