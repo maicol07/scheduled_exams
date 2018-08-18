f4db3320db75bdd62052b9b6b32819fb264c12b4
maicol07 <maicolbattistini@live.it>
2018-08-18 14:06:13 +0200

    Aggiunti messaggi di successo + miglioramento

--- app/index.php
+++ app/index.php
@@ -3,12 +3,28 @@
 /** @noinspection ALL */
 $title = "Dashboard";
 $inc_script = "dashboard";
+$filename = "";
 require_once("layout/header.php");
 ?>
-<body>
 <!-- START NAVBAR -->
 <?php include("layout/navbar.php") ?>
 <!-- END NAVBAR -->
+<?php
+if (isset($_GET["deleteclass"]) and $_GET["deleteclass"] == "success") {
+    echo '<script>swal({
+title: "Classe eliminata",
+text: "La classe ' . $_GET["nome"] . ' è stata eliminata!",
+type: "success" 
+})</script>';
+}
+if (isset($_GET["invite-class"]) and $_GET["invite-class"] == "success") {
+    echo '<script>swal({
+title: "Aggiunto alla classe",
+text: "Sei stato agginto alla classe ' . $_GET["nome"] . '",
+type: "success" 
+})</script>';
+}
+?>
 <!-- START Body -->
 <p align="right" style="padding-right: 20px" class="hide-on-small-only">Benvenuto <?php
     if ($userinfo["nome"] !== "" or $userinfo["cognome"] !== "") {
@@ -37,7 +53,7 @@ require_once("layout/header.php");
             foreach ($listaclassi as $classe) {
                 if (in_array($classe["ID"], $classiutente)) {
                     echo '<div class="col s6 m2 l3">
-                    <div class="card activator hoverable waves-effect waves-light" style="cursor: pointer;" onclick="window.location=classe.php?id=' . $classe["ID"] . '&nome=' . $classe["nome"] . '">
+                    <div class="card activator hoverable waves-effect waves-light" style="cursor: pointer;" onclick="window.location=\'classe.php?id=' . $classe["ID"] . '&nome=' . $classe["nome"] . '\'">
                         <div class="card-action">
                             ' . $classe["nome"] . '
                         </div>
@@ -51,4 +67,3 @@ require_once("layout/header.php");
 <!-- END Body -->
 <!-- Compiled and minified JavaScript -->
 <script src="../js/materialize.min.js"></script>
-</body>
