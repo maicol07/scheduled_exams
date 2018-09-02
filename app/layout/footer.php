071f0d463704217fcf047eb73d860426ebaa11c7
Maicol <maicolbattistini@live.it>
2018-09-02 14:17:23 +0200

    Aggiunto footer + fix responsive

---
+++ app/layout/footer.php
@@ -0,0 +1,41 @@
+<!-- Compiled and minified JavaScript -->
+<script src="../js/materialize.min.js"></script>
+</main>
+</body>
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <a href="index.php" class="brand-logo footer-img" style="font-family: Raleway, sans-serif;"><img
+                            src="../img/logo.svg" alt="Interrogazioni programmate"
+                            width="75" height="75" onerror="this.src='img/logo.png'"><span
+                            class="logo-text footer-logo-text" style="color: white; font-size: 6vw;">Interrogazioni Programmate</span></a>
+                <p class="grey-text text-lighten-4">Gestisci le tue interrogazioni online insieme alla tua classe.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Link utili</h5>
+                <ul style="line-height: 175%">
+                    <li><a class="grey-text text-lighten-3" href="index.php#dashboard"><i class="material-icons left">dashboard</i>Dashboard</a>
+                    </li>
+                    <li><a class="grey-text text-lighten-3" href="index.php#classi"><i
+                                    class="material-icons left">class</i>Classi</a></li>
+                    <li><a class="grey-text text-lighten-3" href="profile.php"><i class="material-icons left">account_circle</i>Profilo</a>
+                    </li>
+                    <li><a class="grey-text text-lighten-3" href="settings.php"><i
+                                    class="material-icons left">settings</i>Impostazioni</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://docs.interrogazioniprogrammate.tk"><i
+                                    class="material-icons left">description</i>Documentazione</a></li>
+                    <li><a class="grey-text text-lighten-3" onclick="info()" style="cursor: pointer"><i
+                                    class="material-icons left">info</i>Informazioni</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Copyright © 2018 Interrogazioni Programmate
+            <a class="grey-text text-lighten-4 right" href="https://interrogazioniprogrammate.tk">Sito web</a>
+        </div>
+    </div>
+</footer>
+</html>
