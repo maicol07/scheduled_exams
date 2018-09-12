071829fb1a60ff3aac7dc58f76659222341de344
Maicol <maicolbattistini@live.it>
2018-09-12 18:14:18 +0200

    Cambiata icona genere in una Material Design Icon SVG

--- app/profile.php
+++ app/profile.php
@@ -146,7 +146,9 @@ if (isset($_GET["user"]) and $_GET["user"] != $_SESSION["username"]) {
                     </li>
                     <li id="genere-li" class="collection-item" ' . $gen . '>
                     <div class="row">
-                        <div class="col s5"><i class="fal fa-transgender"></i> ' . _("Genere") . '</div>
+                        <div class="col s5"><svg class="left" style="width:24px; height:24px; margin-right: 15px" viewBox="0 0 24 24">
+    <path fill="#000000" d="M17.58,4H14V2H21V9H19V5.41L15.17,9.24C15.69,10.03 16,11 16,12C16,14.42 14.28,16.44 12,16.9V19H14V21H12V23H10V21H8V19H10V16.9C7.72,16.44 6,14.42 6,12A5,5 0 0,1 11,7C12,7 12.96,7.3 13.75,7.83L17.58,4M11,9A3,3 0 0,0 8,12A3,3 0 0,0 11,15A3,3 0 0,0 14,12A3,3 0 0,0 11,9Z" />
+</svg>' . _("Genere") . '</div>
                         <div class="col s7 right-align"><span id="genere-content">' . $genere . '</span></div>
                     </div>
                 </li>
