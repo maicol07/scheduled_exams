35a9096c75f1f28fc336474ce5201fb31e3fa000
Maicol Battistini <maicolbattistini@live.it>
2021-11-02 10:24:28 +0100

    feat: ✨ Better footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,14 +15,22 @@
     </material-drawer>
     <footer class="@if(session('high-contrast')) mdc-high-contrast @endif">
         <div class="left-footer">
-            <span>
-                <a href="https://openstamanager.com">
-                    @lang('Scheduled Exams')
-                </a>
-            </span>
+            <span>@lang('Copyright © 2019-%s Maicol07', ['%s' => today()->format('Y')])</span>
+            @notmobile
+            <span> - @lang('Sviluppato da Maicol07')</span>
         </div>
         <div class="right-footer">
-            <b>@lang('Versione')</b> {{trim(file_get_contents(base_path('VERSION')))}}
+            <a href="https://docs.maicol07.it/en/legal/privacy-cookie">
+                <mwc-button label="@lang('Privacy')" dense>
+                    <i class="mdi mdi-file-document-outline" slot="icon"></i>
+                </mwc-button>
+            </a>
+            <a href="https://docs.maicol07.it/en/legal/terms">
+                <mwc-button label="@lang('Termini')" dense>
+                    <i class="mdi mdi-gavel" slot="icon"></i>
+                </mwc-button>
+            </a>
+            @endnotmobile
         </div>
     </footer>
 </top-app-bar>
@@ -33,9 +41,12 @@
 @client
 
 @vite('app')
-
+@php
+@endphp
+<script type="module" async="" src="https://embed.launchnotes.io/latest/dist/esm/launchnotes-embed.js"></script>
+<script nomodule="" async="" src="https://embed.launchnotes.io/latest/dist/esm/launchnotes-embed.js"></script>
 <script>
-    window.translations = JSON.parse('{{file_get_contents(resource_path('lang/'.app()->getLocale().'.json'))}}')
+    window.translations = JSON.parse('{!! Cache::get('translations') !!}')
 </script>
 </body>
 </html>
