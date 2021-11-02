3fa7a739044a8298b8d3e86b471f4fe49a3314f9
Maicol Battistini <maicolbattistini@live.it>
2021-11-02 10:17:03 +0100

    chore: 🍱 Added favicons

--- resources/views/layouts/top-app-bar.blade.php
+++ resources/views/layouts/top-app-bar.blade.php
@@ -1,17 +1,11 @@
 <mwc-icon-button slot="navigationIcon"><i class="mdi mdi-menu"></i></mwc-icon-button>
 <div slot="title" style="display: flex; align-items: center;">
-    <img src="{{vite_asset('images/logo.png')}}" alt="@lang('OpenSTAManager')" style="height: 50px; margin-right: 8px;">
-    <span>@lang('OpenSTAManager')</span>
+    <img src="{{vite_asset('images/favicon/favicon.ico')}}" alt="@lang('Scheduled Exams')" style="margin-right: 8px;">
+    <span>@lang('Scheduled Exams')</span>
 </div>
 
-<mwc-icon-button id="navbar-notifications" slot="actionItems" label="@lang('Notifiche')">
+<mwc-icon-button id="navbar-announcements" slot="actionItems" label="@lang('Notifiche')">
     <i class="mdi mdi-bell-outline"></i>
-</mwc-icon-button>
-<mwc-icon-button id="navbar-print" slot="actionItems" label="@lang('Stampa')">
-    <i class="mdi mdi-printer"></i>
-</mwc-icon-button>
-<mwc-icon-button id="period-switcher" slot="actionItems" label="@lang('Cambia periodo')">
-    <i class="mdi mdi-calendar-range-outline"></i>
 </mwc-icon-button>
 <mwc-icon-button id="user-info-btn" slot="actionItems" label="@lang('Il tuo profilo')">
     @if (Auth::hasUser())
