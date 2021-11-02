b318c60f29429a32aa28751c4eab17321e5b0a3f
Maicol Battistini <maicolbattistini@live.it>
2021-11-02 10:20:18 +0100

    feat: ✨ Added Launchnotes popup

--- resources/views/layouts/top-app-bar-menus.blade.php
+++ resources/views/layouts/top-app-bar-menus.blade.php
@@ -1,15 +1,24 @@
-<mwc-menu activatable corner="BOTTOM_RIGHT" id="notifications-list" trigger="change-language-btn">
-    <p>@lang("{0} Non sono presenti notifiche|{1} C'è una notifica|[2,*] Ci sono :num notifiche", ['num' => 0])</p>
-    @php
-        $notifications = [];
-    @endphp
-    @foreach($notifications as $notification)
-        <mwc-list-item id="notification_{{$notification->id}}" graphic="icon" value="{{$notification->id}}">
-            <i class="mdi mdi-{{$notification->icon}}" slot="graphic"></i>
-            <span>{{$notification->name}}</span>
-        </mwc-list-item>
-    @endforeach
-</mwc-menu>
+<launchnotes-embed
+    token="public_PmygEcoyVxYZFALtlfheHm9l"
+    project="pro_3dqgGLD1mcOgb"
+    categories='["scheduled-exams"]'
+    toggle-selector="#navbar-announcements"
+    heading="@lang('Latest updates')"
+    heading-color="#FFF"
+    subheading="@lang('The latest features, improvements, and bug fixes')"
+    subheading-color="rgba(255, 255, 255, 0.8)"
+    primary-color="#8000ff"
+    widget-placement="auto"
+    widget-offset-skidding="0"
+    widget-offset-distance="0"
+    show-unread
+    anonymous-user-id="{{sha1(auth()->user()->uuid)}}"
+    unread-placement="right-start"
+    unread-offset-skidding="-4"
+    unread-offset-distance="4"
+    unread-background-color="#0080ff"
+    unread-text-color="#ffffff"
+></launchnotes-embed>
 <mwc-menu corner="BOTTOM_LEFT" id="user-info" trigger="user-info-btn">
     @if (Auth::hasUser())
         <img class="mdc-elevation--z2" src="{{auth()->user()->picture}}" alt="{{auth()->user()->username}}"
@@ -18,7 +27,7 @@
         <i class="mdi mdi-account-outline mdc-elevation--z2"></i>
     @endif
     <br>
-    <b style="margin-top: 16px;">{{auth()->user()?->getFullName()}}</b>
+    <b style="margin-top: 16px;">{{auth()->user()?->name}}</b>
     <br>
     <span>{{auth()->user()?->email}}</span>
     <br>
